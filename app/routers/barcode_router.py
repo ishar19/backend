c270ff5d5909ea62349c987a0167ad31df1d6953
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2024-02-06 16:19:58 +0530

    updating system prompt

--- app/routers/barcode_router.py
+++ app/routers/barcode_router.py
@@ -3,6 +3,8 @@ from ..config import settings
 from ..schemas.barcode_details import BarcodeDetails
 from fastapi import APIRouter
 from typing import Any
+from ..dependencies.constants import SYSTEM_PROMPT
+from loguru import logger
 
 barcode_router = APIRouter(
   prefix='/barcode',
@@ -14,4 +16,6 @@ barcode_router = APIRouter(
 async def api_barcode_details(barcode: int) -> dict:
   openfoodfacts_product_url = f'{settings.OPENFOODFACTS_API_URL}/product/{barcode}'
   response = await get_json_response(openfoodfacts_product_url)
+  SYSTEM_PROMPT.format(data=response)
+  logger.info(f"Updated SYSTEM_PROMPT: {SYSTEM_PROMPT}")
   return response
