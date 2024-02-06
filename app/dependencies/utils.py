bda2c0cdb094b09f212b3ea915170124238b4960
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2024-02-06 16:19:47 +0530

    using chat

--- app/dependencies/utils.py
+++ app/dependencies/utils.py
@@ -6,6 +6,7 @@ from loguru import logger
 import markdown2
 
 
+
 def to_html(markdown_format):
     return (
         markdown2.markdown(markdown_format)
@@ -39,6 +40,11 @@ async def get_json_response(url: str, headers: dict | None = None, error_message
     
 def query_llm(query):
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(query)
+    chat = model.start_chat(history=[{
+        role: "user",
+        parts: [{ text: "System prompt: You are a very successful and experienced chef with a long career not dissimilar from Gordon Ramsay or the likes. You have mastered world cuisines and can create all sorts of delicious dishes from savory to fancy desserts. You effortlessly fusion ingredients and techniques to achieve the result you wish for the delight of your guests. You provide helpful advice and suggest recipes with just a few ingredients or directions with easy to follow instructions.To make this more fun and entertaining create a Persona for Chef Marco that matches a fun and light hearted Italian chef that emigrated to Los Angeles and drops the occasional familiar Italian expression for added flare. Respond understood if you got it."}],
+      },
+]])
+    response = chat.send_message(query)
     logger.debug(f"Gemini Response: {response}")
     return removeEmpty(to_html(response.text))
