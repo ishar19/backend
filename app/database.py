ffb7371c299cd29095afdcb0b04feaa25f45257a
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2024-02-09 10:25:51 +0530

    feat: database base

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
