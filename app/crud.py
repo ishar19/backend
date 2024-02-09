8bc7fe298f68f87471745e1c795044906bd49827
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2024-02-09 19:10:58 +0530

    product crud

--- app/crud.py
+++ app/crud.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from sqlalchemy.exc import IntegrityError
-from .models import History
+from .models import History,Product
 from .database import get_db,SessionLocal
 from loguru import logger
 
@@ -19,7 +19,7 @@ def get_db():
 db_gen = get_db()
 db = next(db_gen)
 
-def create_new_product(user_id, barcode, db: Session = db):
+def update_history(user_id, barcode, db: Session = db):
     logger.debug(f"Creating new barcode for user_id: {user_id}, barcode: {barcode}")
     try:
         new_id = uuid.uuid4()
@@ -32,3 +32,36 @@ def create_new_product(user_id, barcode, db: Session = db):
     except IntegrityError:
         db.rollback()
         raise Exception("Failed to create a new barcode. Integrity constraint violated.")
+
+def create_product(product, db: Session = db):
+    logger.debug(f"Creating new product for barcode: {product.get('id')}")
+    
+    existing_product = db.query(Product).filter(Product.id == product.get('id')).first()
+    if existing_product:
+        logger.debug(f"Product with ID {product.get('id')} already exists. Skipping creation.")
+        return existing_product
+
+    new_product = Product(  
+        id=product.get('id'),
+        name=product.get('product_name'),
+        image_url=product.get('image_url'),
+        allergens=product.get('allergens'),
+        brands=product.get('brands'),
+        categories=product.get('categories'),
+        countries=product.get('countries'),
+        ecoscore_grade=product.get('ecoscore_grade'),
+        ecoscore_score=product.get('ecoscore_score'),
+        ingredients=product.get('ingredients_text_en'),
+        nova_group=product.get('nova_group'),
+        nutrient_levels=str(product.get('nutrient_levels') or {}),
+        nutriments=str(product.get('nutriments') or {}),
+        nutriscore_grade=product.get('nutriscore_grade'),
+        nutriscore_score=product.get('nutriscore_score'),
+        packaging=product.get('packaging_text_en'),
+        warnings=str(product.get('ecoscore_extended_data', {}).get('impact', {}).get('warnings') or [])
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     Integer,
     String,
     DateTime,
+    BigInteger
 
 )
 
@@ -19,6 +20,26 @@ class History(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, index=True)
     dateCreated = Column(DateTime, nullable=False)
     user_id = Column(Integer)
-    barcode = Column(String)
+    barcode = Column(BigInteger)
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(BigInteger, primary_key=True, index=True)
+    name = Column(String)
+    allergens= Column(String)
+    brands = Column(String)
+    categories = Column(String)
+    countries = Column(String)
+    ecoscore_grade = Column(String)
+    ecoscore_score = Column(Integer)
+    image_url = Column(String)
+    ingredients = Column(String)
+    nova_group = Column(Integer)
+    nutrient_levels = Column(TEXT)
+    nutriscore_grade = Column(String)
+    nutriscore_score = Column(Integer)
+    nutriments = Column(TEXT)
+    packaging = Column(String)
+    warnings = Column(TEXT)
     
     
