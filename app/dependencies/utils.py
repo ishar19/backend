95642c7bc5739972ff8bf9e8e9ec7cfdf1861aad
Infinil <main8user8@gmail.com>
2024-02-11 13:49:03 +0530

    llm code changed

--- app/dependencies/utils.py
+++ app/dependencies/utils.py
@@ -4,38 +4,13 @@ import httpx
 import google.generativeai as genai
 from loguru import logger
 import markdown2
+from ..models import Product
 from ..schemas.barcode_details import BarcodeDetails
 
 
 SYSTEM_PROMPT = """You are Eco-Friendly and Health Agent.
 This is some {data} about a product in JSON Format. 
 Try to answer questions with reference to this"""
-
-def get_prompt_data(data):
-    logger.debug(f"Data: {data}")
-
-    formatted_data = {
-        "barcode": data.id,
-        "name": data.name,
-        "image_url": data.image_url,
-        "allergens": data.allergens,
-        "brands": data.brands,
-        "categories": data.categories,
-        "countries": data.countries,
-        "ecoscore_grade": data.ecoscore_grade,
-        "ecoscore_score": data.ecoscore_score,
-        "ingredients": data.ingredients,
-        "nova_group": data.nova_group,
-        "nutrient_levels": data.nutrient_levels,
-        "nutriments": data.nutriments,
-        "nutriscore_grade": data.nutriscore_grade,
-        "nutriscore_score": data.nutriscore_score,
-        "packaging": data.packaging,
-        "warnings": data.warnings
-    }
-
-    return formatted_data
-
 
 def to_html(markdown_format):
     return (
@@ -69,12 +44,12 @@ async def get_json_response(url: str, headers: dict | None = None, error_message
       raise HTTPException(status_code=error.response.status_code, detail=error_message)
     
     
-def query_llm(query, data):
-    data = get_prompt_data(data)
+def query_llm(query: str, data: Product):
+    data_dict = data.__dict__
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat(history=[])
     input = f'''
-    SYSTEM_PROMPT:{SYSTEM_PROMPT.format(data=data)} 
+    SYSTEM_PROMPT:{SYSTEM_PROMPT.format(data=data_dict)} 
     user query: {query}
     '''
     logger.debug(f"Gemini Input: {input}")
