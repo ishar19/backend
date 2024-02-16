8742226d431e28400e56cc3863563715785d90d3
ishar19 <isharjain19@gmail.com>
2024-02-17 01:46:42 +0530

    fixed barcode not being sent in general chat

--- app/schemas/llm.py
+++ app/schemas/llm.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class ChatRequestDetails(BaseModel):
-  barcode: Optional[int]
-  user_query: str
+    barcode: Optional[int] = None
+    user_query: str
