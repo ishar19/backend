e8ee36f515a49e88b984a0f850ca4c19aa2d4e52
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2024-02-20 10:07:59 +0530

    Optional image_url

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserCreate(BaseModel):
     id : str
     name : str
     email : str
-    image_url :str
+    image_url :Optional[str] = None
